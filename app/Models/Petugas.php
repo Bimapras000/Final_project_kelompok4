c0a42c91754778c54d45d96f9fd760864884ea55
Bimapras000 <119924269+Bimapras000@users.noreply.github.com>
2023-11-06 22:59:52 +0700

    Final Perpus kel4

---
+++ app/Models/Petugas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Petugas extends Model
+{
+    use HasFactory;
+
+    protected $table = 'petugas';
+        
+    protected $fillable = [
+        'nama','email','username','password'
+    ];
+    
+
+    public function peminjaman(){
+        return $this->hasMany(Peminjaman::class);
+    }
+}
